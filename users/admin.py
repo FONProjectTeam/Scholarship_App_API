aa610df2c938fd5bfa50bb7d8324169b74523049
Ugwoke Edward Chinedu <nedu.stack@gmail.com>
2025-05-17 10:13:51 +0100

    User authentication system

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
 from users.models import User, UserProfile
 
-admin.site.register(User)
 
-admin.site.register(UserProfile)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('get_full_name', 'email', 'user_type', 'phone_number', 'is_active')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    list_filter = ('user_type', 'is_active', 'is_staff')
+    ordering = ('first_name', 'last_name') 
+    list_editable = ('is_active',)
+    
+    def get_full_name(self, obj):
+        return obj.get_full_name()
+    get_full_name.short_description = 'Full Name'
 
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('get_full_name', 'user__email', 'education_level', 'major', 'graduation_year')
+    search_fields = ('user__email', 'user__username', 'user__first_name', 'user__last_name', 'education_level', 'institution', 'major')
+    list_filter = ('education_level', 'major', 'graduation_year')
+    ordering = ('user__email',)
 
+    def get_full_name(self, obj):
+        return obj.user.get_full_name()
+    get_full_name.short_description = 'Full Name'
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 
 
 
+from django.dispatch import receiver
 from shortuuid.django_fields import ShortUUIDField
 
 class User(AbstractUser):
@@ -17,11 +18,14 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
-    other_name = models.CharField(max_length=150, blank=True)
+    other_name = models.CharField(max_length=150, blank=True, null=True)
     username = models.CharField(max_length=150, unique=True, blank=True)
 
     state_of_origin = models.CharField(max_length=50, blank=True)
     lga_of_origin = models.CharField(max_length=100, blank=True)
+
+    otp = models.CharField(max_length=100, blank=True, null=True)
+    otp_created_at = models.DateTimeField(null=True, blank=True)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
@@ -32,20 +36,23 @@ class User(AbstractUser):
     #Create username from email
     def save(self, *args, **kwargs):
         if not self.username:
-            self.username = self.email.split('@')[0]
+            try:
+                self.username = self.email.split('@')[0]
+            except:
+                self.username = self.first_name
 
         super().save(*args, **kwargs)
         
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
-    lcda = models.CharField(max_length=100, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', default="default/default-profule.jpg", null=True, blank=True)
-    address = models.TextField(blank=True)
-    education_level = models.CharField(max_length=100, blank=True)
-    institution = models.CharField(max_length=255, blank=True)
-    major = models.CharField(max_length=255, blank=True)
+    bio = models.TextField(blank=True, null=True)
+    lcda = models.CharField(max_length=100, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', default="default/default-profile.jpg", null=True, blank=True)
+    address = models.TextField(blank=True, null=True)
+    education_level = models.CharField(max_length=100, blank=True, null=True)
+    institution = models.CharField(max_length=255, blank=True, null=True)
+    major = models.CharField(max_length=255, blank=True, null=True)
     graduation_year = models.IntegerField(null=True, blank=True)
     pid = ShortUUIDField(unique=True, length=10, max_length=20, alphabet='0123456789')
 
@@ -56,3 +63,9 @@ class UserProfile(models.Model):
         else:
             return f"{self.user.username}'s Profile"
 
+@receiver(post_save, sender=User)        
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        instance.profile.save()
